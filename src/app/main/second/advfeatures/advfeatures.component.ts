8a048a67619ede6cbb9363532b4c3037f874c633
nikita23022002 <Nikita.J@sankeysolutions.com>
2024-09-29 20:14:36 +0530

    Update advfeatures.component.ts

--- src/app/main/second/advfeatures/advfeatures.component.ts
+++ src/app/main/second/advfeatures/advfeatures.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrl: './advfeatures.component.css'
 })
 export class AdvfeaturesComponent {
+  img_url="/assets/images/graph.svg"
 
+  cards = [
+    {
+      icon: '🛣️',
+      title: 'Route Optimisation',
+      description: 'Maximize efficiency with real-time traffic updates and fuel saving'
+    },
+    {
+      icon: '🚗',
+      title: 'Driver Behaviour Monitoring',
+      description: 'Improve driver behaviour analysis with real-time safety and resource costs'
+    },
+    {
+      icon: '🔧',
+      title: 'Vehicle Health Monitoring',
+      description: 'Reduce Downtime with preventive maintenance alerts and diagnostics'
+    }
+  ];
+
+  activeIndex = 1;
+
+  moveLeft() {
+    this.activeIndex = (this.activeIndex - 1 + this.cards.length) % this.cards.length;
+  }
+
+  moveRight() {
+    this.activeIndex = (this.activeIndex + 1) % this.cards.length;
+  }
 }
