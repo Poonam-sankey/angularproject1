fe0b700f839d9564467935ddef0ebdbda526f141
Poonam <salunkhepoonam274@gmail.com>
2024-10-01 05:08:05 +0530

    "Header edited"

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,20 +1,37 @@
-import { Component } from '@angular/core';
-import { BookService } from '../Services/book.service';
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css',
-  providers: [BookService]
+  styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-    
+export class HeaderComponent implements AfterViewInit {
+  navItems = ['Home', 'Features', 'Pricing', 'Case Studies', 'Contact'];
+  activeIndex = 0;
 
-  constructor( private subBook:BookService)
-  {
+  @ViewChild('underline') underlineElement!: ElementRef;
 
+  constructor(private el: ElementRef) {}
+
+  ngAfterViewInit(): void {
+    this.updateUnderlinePosition();
   }
-  OnBookClicked()
-  {
-    this.subBook. OnBookClicked();
+
+  setActive(index: number): void {
+    this.activeIndex = index;
+    this.updateUnderlinePosition();
+  }
+  // Helper method to convert nav item name to router link path
+  getRouterLink(item: string): string {
+    return `/${item.toLowerCase().replace(/ /g, '-')}`;
+  }
+
+  private updateUnderlinePosition(): void {
+    const navItemsElements = this.el.nativeElement.querySelectorAll('.nav-item');
+    const activeItem = navItemsElements[this.activeIndex] as HTMLElement;
+    if (activeItem && this.underlineElement) {
+      this.underlineElement.nativeElement.style.left = `${activeItem.offsetLeft}px`;
+      this.underlineElement.nativeElement.style.width = `${activeItem.offsetWidth}px`;
+    }
   }
 }
