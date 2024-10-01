27fc5b227db7ecd2657a78e174bb277b46a2b16a
nikita23022002 <Nikita.J@sankeysolutions.com>
2024-10-01 09:06:09 +0530

    Update plan.component.ts

--- src/app/main/first/plan/plan.component.ts
+++ src/app/main/first/plan/plan.component.ts
@@ -7,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrl: './plan.component.css'
 })
 export class PlanComponent {
+  hoveredCard: number | null = null;  // To store which card is currently hovered
 
+  // Function to handle mouse enter
+  onMouseEnter(cardIndex: number): void {
+    this.hoveredCard = cardIndex;  // Set the card index being hovered
+  }
+
+  // Function to handle mouse leave
+  onMouseLeave(): void {
+    this.hoveredCard = null;  // Reset the hovered card when the mouse leaves
+  }
 }
