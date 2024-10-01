e95e86d72962b523256b6bd6d33b18dfb26bdac0
nikita23022002 <Nikita.J@sankeysolutions.com>
2024-10-01 09:19:11 +0530

    Update slide.component.ts

--- src/app/main/first/slide/slide.component.ts
+++ src/app/main/first/slide/slide.component.ts
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrl: './slide.component.css'
 })
 export class SlideComponent {
-  graphImg_url="/assets/images/stacked-graph.png"
+  graphImg_url = "/assets/images/stacked-graph.png";
+
+  // Common default value for all sliders
+  defaultValue: number = 5;
+
+  // Slider values (set to defaultValue)
+  fleetSize: number = this.defaultValue;
+  kmPerDay: number = this.defaultValue;
+  avgKmPerL: number = this.defaultValue;
+  hourlyWages: number = this.defaultValue;
+  dailyDeliveries: number = this.defaultValue;
+  workingHours: number = this.defaultValue;
+
+  // Methods to update slider values
+  onFleetSizeChange(event: any) {
+    this.fleetSize = event.target.value;
+  }
+
+  onKmPerDayChange(event: any) {
+    this.kmPerDay = event.target.value;
+  }
+
+  onAvgKmPerLChange(event: any) {
+    this.avgKmPerL = event.target.value;
+  }
+
+  onHourlyWagesChange(event: any) {
+    this.hourlyWages = event.target.value;
+  }
+
+  onDailyDeliveriesChange(event: any) {
+    this.dailyDeliveries = event.target.value;
+  }
+
+  onWorkingHoursChange(event: any) {
+    this.workingHours = event.target.value;
+  }
 }
